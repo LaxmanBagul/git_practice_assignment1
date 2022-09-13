d6d20d732a5727247c6bb45278a6ccc18c5529fa
Yogesh <patilyogesh2801@gmail.com>
2022-09-13 20:34:22 +0530

    Fixed Code

--- string_palindrom_or.js
+++ string_palindrom_or.js
@@ -1,2 +1,11 @@
-for(let a=1;a<=N.length-1;a++){
-   
+//check string is palimdrome or not print Yes if it palindome else print No
+let str2="";
+let str="naman";
+for(i=N-1;i>=0;i--){
+       str2=str2+str[i];
+}
+    if(str==str2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
